2f177b050aabd66e8f86053d0ac7e2062acc3822
Jeff Reiner <theundeadghost@gmail.com>
2021-12-27 18:49:35 +0100

    Adding Tailwind

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,14 @@
+<script lang="ts">
+  import "../app.css";
+  import { page } from "$app/stores";
+
+  $: isActive = (href: string) => $page.path === href;
+</script>
+
 <nav>
   <ul>
-    <li><a href="/">/</a></li>
-    <li><a href="/vinyl">/vinyl</a></li>
+    <li><a href="/">{isActive("/") ? "[x]" : "[ ]"} Home</a></li>
+    <li><a href="/vinyl">{isActive("/vinyl") ? "[x]" : "[ ]"} Vinyl</a></li>
   </ul>
 </nav>
 
